937f964f65b17d65d8bcd24b3be5af35aee0455b
gdharani1996 <gdharanidharan07@gmail.com>
2022-12-16 12:37:14 +0530

    mid_commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
-import os, sys
+import os
+import sys
 from sensor.logger import logging
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_utils import read_yaml_file
 
 
 if __name__=="__main__":
-    train_pipeline = TrainPipeline()
-    train_pipeline.run_pipeline()
+
+    try:
+        train_pipeline = TrainPipeline()
+        train_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
+
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -4,6 +4,7 @@ from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
 class TrainPipeline:
     def __init__(self):
@@ -11,7 +12,7 @@ class TrainPipeline:
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
-            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config = self.training_pipeline_config)
             logging.info("Starting data ingestion")
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
@@ -21,6 +22,18 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
 
+
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config = self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact = data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except  Exception as e:
+            raise SensorException(e,sys)
+            
 
     def run_pipeline(self):
         try:
