d8d014e92f4383632de652482d64e2d7fce905d7
gdharani1996 <gdharanidharan07@gmail.com>
2022-12-16 12:17:07 +0530

    mid_commit

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -12,23 +12,46 @@ class TrainingPipelineConfig:
 
 
 class DataIngestionConfig:
-        def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-            self.data_ingestion_dir: str = os.path.join(
-                training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME 
-            ) # artifact/timestamp/data_ingestion
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_ingestion_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME 
+        ) # artifact/timestamp/data_ingestion
 
-            self.feature_store_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
-            ) # artifact/timestamp/data_ingestion/feature_store/sensor.csv
+        self.feature_store_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+        ) # artifact/timestamp/data_ingestion/feature_store/sensor.csv
 
-            self.training_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
-            ) # artifact/timestamp/data_ingestion/ingested/train.csv
+        self.training_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
+        ) # artifact/timestamp/data_ingestion/ingested/train.csv
 
-            self.testing_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
-            ) # artifact/timestamp/data_ingestion/ingested/test.csv
+        self.testing_file_path: str = os.path.join(
+            self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+        ) # artifact/timestamp/data_ingestion/ingested/test.csv
 
-            self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION # 0.2
-            self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME # sensor
+        self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION # 0.2
+        self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME # sensor
+
+
+class DataValidationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME
+        ) # artifact/timestamp/data_validation
+        self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR
+        ) # artifact/timestamp/data_validation/valid
+        self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR
+        ) # artifact/timestamp/data_validation/invalid
+        self.valid_train_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME
+        ) # artifact/timestamp/data_validation/valid/train.csv
+        self.valid_test_file_path: str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME
+        ) # artifact/timestamp/data_validation/valid/test.csv
+        self.invalid_train_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME
+        ) # artifact/timestamp/data_validation/invalid/train.csv
+        self.invalid_test_file_path: str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME
+        ) # artifact/timestamp/data_validation/invalid/test.csv
+        self.drift_report_file_path: str = os.path.join(
+            self.data_validation_dir,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
+            training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
+        ) # artifact/timestamp/data_validation/drift_report/report.yaml     
 
