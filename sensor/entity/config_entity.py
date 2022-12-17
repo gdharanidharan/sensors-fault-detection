8f314914382fe15e0caf497b56144743f4ea11fb
gdharani1996 <gdharanidharan07@gmail.com>
2022-12-17 19:37:31 +0530

    mid_above_commit

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -80,3 +80,18 @@ class DataTransformationConfig:
             training_pipeline.PREPROCSSING_OBJECT_FILE_NAME
         ) # artifact/timestamp/data_transformation/transformed_object/preprocessing.pkl                     
 
+
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, training_pipeline.MODEL_TRAINER_DIR_NAME
+        ) # artifact/timestamp/model_trainer
+        
+        self.trained_model_file_path: str = os.path.join(
+            self.model_trainer_dir, training_pipeline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
+            training_pipeline.MODEL_FILE_NAME
+        ) # artifact/timestamp/model_trainer/trained_model/model.pkl
+
+        self.expected_accuracy: float = training_pipeline.MODEL_TRAINER_EXPECTED_SCORE # 0.6
+        self.overfitting_underfitting_threshold = training_pipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD # 0.05
+
